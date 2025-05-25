dbb988674c518691162e4f1c7441ca147d92407e
toothlessdev <toothless042@gmail.com>
2025-05-25 21:58:30 +0900

    feature: implement pokemon list page

--- src/pages/PokemonListPage.jsx
+++ src/pages/PokemonListPage.jsx
@@ -1,3 +1,25 @@
+import { PokemonCard } from "@/components/PokemonCard";
+import { PokemonDashboard } from "@/components/PokemonDashboard";
+import * as Styles from "./PokemonListPage.styled";
+import { pokemonData } from "@/__mocks__";
+
 export const PokemonListPage = () => {
-    return <div>list</div>;
+    return (
+        <Styles.PageContainer>
+            <PokemonDashboard></PokemonDashboard>
+
+            <Styles.PokemonList>
+                {pokemonData.map((pokemon) => {
+                    return (
+                        <PokemonCard
+                            key={pokemon.id}
+                            id={pokemon.id}
+                            name={pokemon.korean_name}
+                            image={pokemon.img_url}
+                        />
+                    );
+                })}
+            </Styles.PokemonList>
+        </Styles.PageContainer>
+    );
 };
