499333810f841c37b937ffb1a4a0de14f5b2c9ea
toothlessdev <toothless042@gmail.com>
2025-05-25 21:56:53 +0900

    feature: implement home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import * as Styles from "./HomePage.styled";
+import logo from "@/assets/logo.png";
+
 export const HomePage = () => {
-    return <div>home</div>;
+    const navigate = useNavigate();
+
+    return (
+        <Styles.PageContainer>
+            <Styles.LogoImage src={logo} alt="포켓몬 도감" />
+            <Styles.Button onClick={() => navigate("/dex")}>포켓몬 도감 시작하기</Styles.Button>
+        </Styles.PageContainer>
+    );
 };
